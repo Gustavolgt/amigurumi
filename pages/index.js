f6d56415d560b4271ceeb127cc563099cafcccba
Gustavolgt <gustavocms16@gmail.com>
2021-04-16 16:04:20 -0400

    add Fb pixel

--- pages/index.js
+++ pages/index.js
@@ -113,6 +113,11 @@ const Home = () => {
                 />
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma-carousel@4.0.3/dist/css/bulma-carousel.min.css" />
+                <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-55FTBZN');`}}></script>
             </Head>
 
             <main className="">
