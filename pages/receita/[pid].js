732913d6e19604ff931c4e702dafb9ac37875eea
Gustavolgt <gustavocms16@gmail.com>
2021-06-02 08:31:51 -0400

    novo pid

--- pages/receita/[pid].js
+++ pages/receita/[pid].js
@@ -15,10 +15,9 @@ export async function getServerSideProps({params}) {
 }
 
 const Post = ({post}) => {
-    const router = useRouter()
-    const { pid } = router.pid
-    const src = router.src
-    const baseUrl = 'https://www.amigurumimeu.com/desconto'
+    const { query } = useRouter()
+    var src = query.src
+    const baseUrl = '/desconto'
     const url = baseUrl + '?src=' + src
     const receita = post[0].content.rendered
     const receita2 = receita.replace("\n","<br/>")
@@ -73,11 +72,6 @@ const Post = ({post}) => {
                 <title>Arts de Amigurumi</title>
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossOrigin="anonymous"></link>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-                <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-55FTBZN');`}}></script>
             </Head>
             <main className="text-center mt-4">
                 <Image
